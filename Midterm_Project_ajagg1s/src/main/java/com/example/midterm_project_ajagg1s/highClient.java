73dfca2e478e887aad50f11f6f7a4fed260d59dc
Aryaman Jaggi <90717745+AryamanJaggi@users.noreply.github.com>
2022-09-28 12:04:03 -0400

    Add files via upload

--- Midterm_Project_ajagg1s/src/main/java/com/example/midterm_project_ajagg1s/highClient.java
+++
@@ -1,8 +0,0 @@
-package com.example.midterm_project_ajagg1s;
-
-public class highClient extends client {
-    public highClient(String name) {
-        this.name = name;
-        this.volume = "high volume";
-    }
-}
--- Midterm_Project_ajagg1s/src/main/java/com/example/midterm_project_ajagg1s/lowClient.java
+++
@@ -1,9 +0,0 @@
-package com.example.midterm_project_ajagg1s;
-
-public class lowClient extends client {
-    public lowClient(String name) {
-        this.name = name;
-        this.volume = "low volume";
-    }
-}
-
